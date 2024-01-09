0221a021043a206e158badc46676ace878dad8e4
nu <yuuta_hiramatsu0227@yahoo.co.jp>
2024-01-10 02:31:05 +0900

    updated code_market(added topix)

--- code_market.py
+++ code_market.py
@@ -5,6 +5,10 @@ codes_prime = [code_info[2][:-1] for code_info in codes_info if code_info[11] ==
 codes_standard = [code_info[2][:-1] for code_info in codes_info if code_info[11] == "スタンダード"]
 codes_grows = [code_info[2][:-1] for code_info in codes_info if code_info[11] == "グロース"]
 
+codes_topix100 = [code_info[2][:-1] for code_info in codes_info if code_info[9] == "TOPIX Core30" or code_info[9] == "TOPIX Large70"]
+codes_topix500 = [code_info[2][:-1] for code_info in codes_info if code_info[9] == "TOPIX Core30" or code_info[9] == "TOPIX Large70" or code_info[9] == "TOPIX Mid400"]
+
+
 PREFIX = "datas/code_market/"
 def writes(contents, filename):
     open(PREFIX + filename, "w").write(", ".join(contents))
@@ -12,3 +16,5 @@ def writes(contents, filename):
 writes(codes_prime, "prime.csv")
 writes(codes_standard, "standard.csv")
 writes(codes_grows, "grows.csv")
+writes(codes_topix100, "topix100.csv")
+writes(codes_topix500, "topix500.csv")
